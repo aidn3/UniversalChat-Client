5714633d83c7a2a42c1333cf48af7f7f303152c3
aidn5 <aydin.atrakchi@gmail.com>
2022-08-05 20:41:18 +0200

    fix mixins crashing the launcher minecraft core isn't loaded yet in that stage. a workaround is to extract all minecraft related code into separate PlayerCallback.java and reflect to it and add it via asm

--- src/main/java/com/aidn5/universalchat/mixins/MinecraftMessageMixin.java
+++ src/main/java/com/aidn5/universalchat/mixins/MinecraftMessageMixin.java
@@ -1,18 +1,11 @@
 package com.aidn5.universalchat.mixins;
 
-import com.aidn5.universalchat.UniversalChat;
-import com.aidn5.universalchat.common.Channel;
-import com.aidn5.universalchat.common.MessageUtil;
 import com.aidn5.universalchat.mixins.tools.MixinUtil;
 import com.aidn5.universalchat.mixins.tools.TransformerName;
 import com.aidn5.universalchat.mixins.tools.TransformerTarget;
-import com.aidn5.universalchat.server.packets.Message;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.MethodNode;
 
-import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
@@ -39,7 +32,7 @@ public class MinecraftMessageMixin extends TransformerTarget {
 
     @Override
     public void transform(MethodNode mn) {
-        Method method = Arrays.stream(MinecraftMessageMixin.class.getMethods())
+        Method method = Arrays.stream(PlayerCallback.class.getMethods())
                 .filter(m -> m.getName().equals("onChatMessage"))
                 .findFirst().get();
 
@@ -47,30 +40,5 @@ public class MinecraftMessageMixin extends TransformerTarget {
         MixinUtil.injectMethodCall(mn, method, loadArgs);
     }
 
-    // return true to exit method, called by asm from #transform
-    @SuppressWarnings("unused")
-    public static boolean onChatMessage(String message) {
-        if (message.startsWith("/")) return false;
-        if (UniversalChat.configInstance.currentChannel == Channel.NONE) return false;
 
-        if (!UniversalChat.configInstance.showGlobalChat) {
-            UniversalChat.configInstance.currentChannel = Channel.NONE;
-            Minecraft.getMinecraft().thePlayer.addChatMessage(MessageUtil.getBackDefaultMessage());
-
-            try {
-                UniversalChat.configInstance.save();
-            } catch (IOException e) {
-                e.printStackTrace();
-                Minecraft.getMinecraft().thePlayer.addChatMessage(MessageUtil.getErrorSavingSettings());
-            }
-
-            return false;
-        }
-
-        EntityPlayerSP p = Minecraft.getMinecraft().thePlayer;
-        String displayName = null;
-        Message messagePacket = new Message(p.getName(), displayName, message, true);
-        UniversalChat.socketHook.sendMessage(messagePacket);
-        return true;
-    }
 }
