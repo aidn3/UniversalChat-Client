2602ccc5eb33125a7c578b5350e47cc28e43a3bf
aidn3 <aidn.david3@gmail.com>
2022-09-23 17:40:14 +0200

    up the version from 1.0 to 1.1

--- src/main/java/com/aidn5/universalchat/UniversalChat.java
+++ src/main/java/com/aidn5/universalchat/UniversalChat.java
@@ -31,7 +31,7 @@ import java.net.URISyntaxException;
 )
 public class UniversalChat {
     public static final String MODID = "UniversalChat";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     public static ConfigInstance configInstance;
     public static SocketHook socketHook;
